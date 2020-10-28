e1dbec74bb0e584f06bcd991c865998891334864
Samantha Bracellari <sbracellari@gmail.com>
2020-10-28 07:27:31 -0400

    fixes formatting #36

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -83,8 +83,8 @@ export default function Calendar(props) {
                 year: 'numeric',
                 month: 'long',
               },
-            fixedWeekCount: false,
-            weekNumbers: false
+              fixedWeekCount: false,
+              weekNumbers: false
             },
             timeGridWeek: {
               titleFormat: {
